07b49af64d316a4cff8a3eb3c475b8e6b48f57c5
unknown <catalan.allendes@gmail.com>
2022-07-08 02:23:58 -0400

    Subida FrontEnd conectado API

---
+++ functions.js
@@ -0,0 +1,43 @@
+function getProductList(){
+    fetch('http://localhost:8080/api/product/list')
+    .then(response => response.json())
+    .then(data => CreateTableFromJSON(data));
+}
+
+function CreateTableFromJSON(data){
+    
+    
+    let col = [];
+    for (let i = 0; i < data.length; i++) {
+        for (let key in data[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+        
+    }
+
+    let table = document.createElement("table");
+
+    let tr = table.insertRow(-1);
+
+    for (let i = 0; i < col.length; i++) {
+        let th = document.createElement("th");
+        th.innerHTML = col[i];
+        tr.appendChild(th);        
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        tr = table.insertRow(-1);
+
+        for (let j = 0; j < col.length; j++) {
+            let tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = data[i][col[j]];
+        }        
+    }
+
+    let divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
+
